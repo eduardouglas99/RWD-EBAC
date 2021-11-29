cd4f79fd1f4e1ad0df3d5e9269ab9f81de01dd61
Eduardo Douglas de Melo Caetano <ecaetano@marlin.net>
2021-11-28 22:43:08 -0300

    adciona api e imagens

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -12,77 +12,90 @@ import iconPisoTatil from '../../images/icon-piso-tatil.png';
 import iconPortaLarga from '../../images/icon-porta-larga.png';
 import iconRampa from '../../images/icon-rampa.png';
 
-const Card = () => {
+const Card = ({item}) => {
+    const imagePath = require('../../images' + item.image)
     return(
         <div className="card__container">
-            <div className="card__image"></div>
+            <div className="card__image">
+                <img src={imagePath.default} alt={item.name} title={item.name}></img>
+            </div>
             <div className="card__content">
-                <h3>Nome do local</h3>
+                <h3>{item.name}</h3>
                 <ul className="card__facilities">
-                    <li className="card__facilities__item">
+                    <li className={`card__facilities__item ${item.facilities[0]} 
+                    ${!item.facilities[0].status ? 'card__facilities__item--unchecked' : ''}`}>
                         <img
                             src={iconBraile}
                             alt="Sinalização em Braile"
                             title="Sinalização em Braile"
                         />
                     </li>
-                    <li className="card__facilities__item card__facilities__item--unchecked">
+                    <li className={`card__facilities__item ${item.facilities[1]} 
+                    ${!item.facilities[1].status ? 'card__facilities__item--unchecked' : ''}`}>
                         <img
                             src={iconAudio}
                             alt="Audio descrição"
                             title="Audio descrição"
                         />
                     </li>
-                    <li className="card__facilities__item">
+                    <li className={`card__facilities__item ${item.facilities[2]} 
+                    ${!item.facilities[2].status ? 'card__facilities__item--unchecked' : ''}`}>
                         <img
                             src={iconBanheiro}
                             alt="Banheiro acessível"
                             title="Banheiro acessível"
                         />
                     </li>
-                    <li className="card__facilities__item">
+                    <li className={`card__facilities__item ${item.facilities[3]} 
+                    ${!item.facilities[3].status ? 'card__facilities__item--unchecked' : ''}`}>
                         <img
                             src={iconElevador}
                             alt="Elevador para cadeirantes"
                             title="Elevador para cadeirantes"
                         />
                     </li>
-                    <li className="card__facilities__item">
+                    <li className={`card__facilities__item ${item.facilities[4]} 
+                    ${!item.facilities[4].status ? 'card__facilities__item--unchecked' : ''}`}>
                         <img
                             src={iconEstacionamento}
                             alt="Estacionamento para idosos / deficientes"
                             title="Estacionamento para idosos / deficientes"
                         />
                     </li>
-                    <li className="card__facilities__item">
+                    <li className={`card__facilities__item ${item.facilities[5]} 
+                    ${!item.facilities[5].status ? 'card__facilities__item--unchecked' : ''}`}>
                         <img
                             src={iconCaoguia}
                             alt="Permitir acesso de cães-guias"
                             title="Permitir acesso de cães-guias"
                         />
                     </li>
-                    <li className="card__facilities__item">
+                    <li className={`card__facilities__item ${item.facilities[6]} 
+                    ${!item.facilities[6].status ? 'card__facilities__item--unchecked' : ''}`}>
                         <img
                             src={iconPisoTatil}
                             alt="Piso tátil de alerta"
                             title="Piso tátil de alerta"
                         />
                     </li>
-                    <li className="card__facilities__item card__facilities__item--unchecked">
+                    <li className={`card__facilities__item ${item.facilities[7]} 
+                    ${!item.facilities[7].status ? 'card__facilities__item--unchecked' : ''}`}>
                         <img
                             src={iconPortaLarga}
                             alt="Portas largas"
                             title="Portas largas"
                         />
                     </li>
-                    <li className="card__facilities__item">
+                    <li className={`card__facilities__item ${item.facilities[8]} 
+                    ${!item.facilities[8].status ? 'card__facilities__item--unchecked' : ''}`}>
                         <img
                             src={iconRampa}
                             alt="Rampas de acesso para cadeirante"
                             title="Rampas de acesso para cadeirante"
                         />
                     </li>
-                    <li className="card__facilities__item card__facilities__item--unchecked">
+                    <li className={`card__facilities__item ${item.facilities[9]} 
+                    ${!item.facilities[9].status ? 'card__facilities__item--unchecked' : ''}`}>
                         <img
                             src={iconLibras}
                             alt="Atendimento em libras"
@@ -90,7 +103,7 @@ const Card = () => {
                         />
                     </li>
                 </ul>
-                <p>lorem ipsum dolor sit amet consectetur adpisicing elit.</p>
+                <p>{item.description}</p>
                 <Button>Como chegar</Button>
             </div>
         </div>
--- src/components/Slider/index.js
+++ src/components/Slider/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import { FilterContext } from '../../contexts/FilterContext';
 import SwiperCore, {Pagination} from 'swiper';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import Card from '../Card';
+import api from '../../config/api';
 
 import 'swiper/swiper-bundle.css'
 
-SwiperCore.use(Pagination)
+SwiperCore.use([Pagination])
 
 function Slider() {
+    const { filteredPlaces, setFilteredPlaces } = useContext(FilterContext);
+    const [places, setPlaces] = useState([]);
+
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            const result = await api.get(`/places?category=${filteredPlaces}`)
+
+            console.log(result)
+
+            if (result.status === 200) {
+                setPlaces(result.data);
+            }
+        }
+
+        fetchPlaces();
+    }, [filteredPlaces])
+
     return(
-        <Swiper slidesPerView={1}
+        <Swiper
             breakpoints={
                 {
+                    300: {
+                        slidesPerView: 1
+                    },
                     767: {
                         slidesPerView: 2
                     },
@@ -22,30 +44,13 @@ function Slider() {
             }
         
         >
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Card/>
-            </SwiperSlide>
+            {
+                places.map(item => (
+                    <SwiperSlide key={item.id} >
+                        <Card key={item.id} item={item}/>
+                    </SwiperSlide>
+                ))
+            }
 
         </Swiper>
     )
